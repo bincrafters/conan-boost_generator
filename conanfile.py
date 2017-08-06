86c1c6593eba51f1c6e95dccce93a2635851b283
Jerry W <jerrywiltse@gmail.com>
2017-08-05 20:47:22 -0400

    string corrections per rene

--- conanfile.py
+++ conanfile.py
@@ -48,42 +48,31 @@ class boost(Generator):
                 .replace("{{{deps.include_paths}}}", jam_include_paths) \
                 .replace("{{{os}}}", self.b2_os()) \
                 .replace("{{{address_model}}}", self.b2_address_model()) \
-                .replace("{{{architecture}}}", self.b2_architecture())   \
-                .replace("{{{toolset}}}", self.b2_toolset())
-            # toolset replace currently does nothing because rene said it should be in cli
-            # I don't understand why, so leaving it here until i can ask and make sure
+                .replace("{{{architecture}}}", self.b2_architecture())
             return jamroot_content
 
     def b2_os(self):
         b2_os = {
-            'Windows': 'target-os=windows',
-            'Linux': 'target-os=linux',
-            'Macos': 'target-os=darwin',
-            'Android': 'target-os=android',
-            'iOS': 'target-os=iphone',
-            'FreeBSD': 'target-os=freebsd',
-            'SunOS': 'target-os=solaris'}
+            'Windows': 'windows',
+            'Linux': 'linux',
+            'Macos': 'darwin',
+            'Android': 'android',
+            'iOS': 'iphone',
+            'FreeBSD': 'freebsd',
+            'SunOS': 'solaris'}
         return b2_os[str(self.settings.os)]
 
     def b2_address_model(self):
         b2_address_model = {
-            'x86': 'address-model=32',
-            'x86_64': 'address-model=64',
-            'ppc64le': 'address-model=64',
-            'ppc64': 'address-model=64',
-            'armv6': 'address-model=32',
-            'armv7': 'address-model=32',
-            'armv7hf': 'address-model=32',
-            'armv8': 'address-model=64'}
+            'x86': '32',
+            'x86_64': '64',
+            'ppc64le': '64',
+            'ppc64': '64',
+            'armv6': '32',
+            'armv7': '32',
+            'armv7hf': '32',
+            'armv8': '64'}
         return b2_address_model[str(self.settings.arch)]
-
-    def b2_toolset(self):
-        b2_toolset = {
-            'gcc': 'toolset=gcc',
-            'Visual Studio': 'toolset=msvc',
-            'clang': 'toolset=clang',
-            'apple-clang': 'toolset=darwin'}
-        return b2_toolset[str(self.settings.compiler)]
 
     def b2_architecture(self):
         if str(self.settings.arch).startswith('x86'):
