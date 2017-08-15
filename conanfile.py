8083491bb5aaed3fb95013436e4d22e825a6da20
Jerry W <jerrywiltse@gmail.com>
2017-08-15 16:37:11 -0400

    fixed looping through shortnames

--- conanfile.py
+++ conanfile.py
@@ -83,9 +83,10 @@ class boost(Generator):
                 deps_info.append(
                     "use-project /" + dep_name +  " : " + dep_libdir.replace('\\','/') + " ;")
                 try:
-                    dep_short_name = self.conanfile.deps_user_info[dep_name].lib_short_name
-                    deps_info.append(
-                        'LIBRARY_DIR(' + dep_short_name + ') = "' + dep_libdir.replace('\\','/') + '" ;')
+                    dep_short_names = self.conanfile.deps_user_info[dep_name].lib_short_names
+                    for dep_short_name in dep_short_names:
+                        deps_info.append(
+                            'LIBRARY_DIR(' + dep_short_name + ') = "' + dep_libdir.replace('\\','/') + '" ;')
                 except KeyError:
                     pass
 
