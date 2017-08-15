931d4f07dfe87ef545fe20f47fef40f90c847923
Jerry W <jerrywiltse@gmail.com>
2017-08-15 16:11:35 -0400

    moved logic to methods - removed single-lib if-then cases

--- conanfile.py
+++ conanfile.py
@@ -29,62 +29,69 @@ class boost(Generator):
 
     @property
     def content(self):
+        jam_include_paths = ' '.join('"' + path + '"' for path in self.conanfile.deps_cpp_info.includedirs).replace('\\','/')
+     
+        libraries_to_build = " ".join(self.conanfile.lib_short_names)
 
-        conanfile = self.conanfile
-        jam_include_paths = ' '.join('"' + path + '"' for path in conanfile.deps_cpp_info.includedirs).replace('\\','/')
-        boost_build = conanfile.deps_cpp_info["Boost.Build"]
+        jamroot_content = self.get_template_content() \
+            .replace("{{{libraries}}}", libraries_to_build) \
+            .replace("{{{boost_version}}}", self.conanfile.version) \
+            .replace("{{{deps.include_paths}}}", jam_include_paths) \
+            .replace("{{{os}}}", self.b2_os()) \
+            .replace("{{{address_model}}}", self.b2_address_model()) \
+            .replace("{{{architecture}}}", self.b2_architecture()) \
+            .replace("{{{deps_info}}}", self.get_deps_info_for_jamfile()) \
+            .replace("{{{variant}}}", self.b2_variant()) \
+            .replace("{{{name}}}", self.conanfile.name)
+            
+           
+        return {
+            "jamroot" : jamroot_content,
+            "boostcpp.jam" : self.get_boostcpp_content(), 
+            "boost-build.jam" : self.get_boost_build_jam_content(),
+            "project-config.jam" : ""
+        }
+
+    def get_boost_build_jam_content(self):
+        boost_build = self.conanfile.deps_cpp_info["Boost.Build"]
         boost_build_root_path = boost_build.rootpath
         boost_build_kernel_path = os.path.join(boost_build_root_path, "share/boost-build/src/kernel").replace('\\','/')
         boost_build_jam_content = 'boost-build "' + boost_build_kernel_path + '" ;'
+        return boost_build_jam_content
+
+    def get_template_content(self):
+        template_file_path = os.path.join(self.get_boost_generator_source_path(), "jamroot.template")
+        template_content = load(template_file_path)
+        return template_content
+
+    def get_boostcpp_content(self):
+        boostcpp_file_path = os.path.join(self.get_boost_generator_source_path(), "boostcpp.jam")
+        boostcpp_content = load(boostcpp_file_path)
+        return boostcpp_content
         
-        boost_generator = conanfile.deps_cpp_info["Boost.Generator"]
+    def get_boost_generator_source_path(self):
+        boost_generator = self.conanfile.deps_cpp_info["Boost.Generator"]
         boost_generator_root_path = boost_generator.rootpath
         boost_generator_source_path = os.path.join(boost_generator_root_path, os.pardir, os.pardir, "export")
+        return boost_generator_source_path
         
-        template_file_path = os.path.join(boost_generator_source_path, "jamroot.template")
-        boostcpp_file_path = os.path.join(boost_generator_source_path, "boostcpp.jam")
-        
-        template_content = load(template_file_path)
-        boostcpp_content = load(boostcpp_file_path)
-
+    def get_deps_info_for_jamfile(self):
         deps_info = []
-
         for dep_name, dep_cpp_info in self.deps_build_info.dependencies:
             dep_libdir = os.path.join(dep_cpp_info.rootpath, dep_cpp_info.libdirs[0])
             if os.path.isfile(os.path.join(dep_libdir,"jamroot.jam")):
                 deps_info.append(
                     "use-project /" + dep_name +  " : " + dep_libdir.replace('\\','/') + " ;")
                 try:
-                    dep_short_name = conanfile.deps_user_info[dep_name].lib_short_name
+                    dep_short_name = self.conanfile.deps_user_info[dep_name].lib_short_name
                     deps_info.append(
                         'LIBRARY_DIR(' + dep_short_name + ') = "' + dep_libdir.replace('\\','/') + '" ;')
                 except KeyError:
                     pass
 
         deps_info = "\n".join(deps_info)
-
-        if hasattr(conanfile, 'lib_short_name'):
-            libraries = conanfile.lib_short_name
-        else:
-            libraries = " ".join(conanfile.lib_short_names)
-
-        jamroot_content = template_content \
-            .replace("{{{libraries}}}", libraries) \
-            .replace("{{{boost_version}}}", conanfile.version) \
-            .replace("{{{deps.include_paths}}}", jam_include_paths) \
-            .replace("{{{os}}}", self.b2_os()) \
-            .replace("{{{address_model}}}", self.b2_address_model()) \
-            .replace("{{{architecture}}}", self.b2_architecture()) \
-            .replace("{{{deps_info}}}", deps_info) \
-            .replace("{{{variant}}}", self.b2_variant()) \
-            .replace("{{{name}}}", conanfile.name)
-            
-        return {
-            "boostcpp.jam" : boostcpp_content, 
-            "jamroot" : jamroot_content,
-            "boost-build.jam" : boost_build_jam_content
-        }
-
+        return deps_info
+        
     def b2_os(self):
         b2_os = {
             'Windows': 'windows',
